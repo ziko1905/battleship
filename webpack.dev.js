fc4c2b860116dff3a6b8727c363cd4420d8a6180
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-04 23:16:46 +0200

    Install babel(and core)
    Install babel and its loader
    
    Install babel(and core and loader)

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,4 +7,20 @@ module.exports = merge(common, {
   devServer: {
     static: "./dist",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }  
 });
