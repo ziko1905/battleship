46b4e46c7ece246cf6f602e316e86e3e912f36f6
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-05 03:05:19 +0200

    Create and test GameBoard

--- src/logic.js
+++ src/logic.js
@@ -11,3 +11,57 @@ export class Ship {
         return (!this.hp)
     }
 }
+
+export class GameBoard {
+    static #BOARD_SIZE = 10
+    constructor () {
+        // Board represented as m * n grid
+        for (let m = 0; m < GameBoard.#BOARD_SIZE; m++) {
+            this[m] = {}
+            for (let n = 0; n < GameBoard.#BOARD_SIZE; n++) { 
+                this[m][n] = new BoardCell()
+            }
+        }
+    }
+    place (m, n, length) {
+        this.checkPlace(m, n)
+        this.checkLength(n, length)
+        let _cells = []
+        for (let i = 0; i < length; i++){
+            try {
+                this.checkPlace(m, n+i)
+                this[m][n+i].makeShip()
+                _cells.push(this[m][n+i])
+            } catch (error) {
+                _cells.forEach((cell) => cell.unmakeShip())
+                throw error
+            }
+        } 
+    }
+    isShip (m, n) {
+        return this[m][n].isShip()
+    }
+    checkPlace (m, n) {
+        if ((m < 0 || m > GameBoard.#BOARD_SIZE - 1) || (n < 0 || n > GameBoard.#BOARD_SIZE - 1)) throw new Error("Wrong ship coordinates") 
+        else if (this[m][n].isShip()) throw new Error("Cell already occupied")
+    }
+    checkLength (n, length) {
+        if (length < 1 || n + length - 1 > GameBoard.#BOARD_SIZE) throw new Error("Wrong ship length")
+    }
+}
+
+class BoardCell {
+    constructor () {
+        this.checked = false
+        this.ship = false
+    }
+    makeShip () {
+        this.ship = true
+    }
+    unmakeShip () {
+        this.ship = false
+    }
+    isShip () {
+        return this.ship
+    }
+}
--- src/logic.test.js
+++ src/logic.test.js
@@ -1,20 +1,24 @@
-import { experiments } from "webpack";
+/* eslint-disable no-undef */
 import { Ship } from "./logic";
+import { GameBoard } from "./logic";
 
 describe("Ships", () => {
     let ship;
     beforeEach(() => {
         ship = new Ship(3);
     })
+
     test("Ship exists", () => {
         expect(ship).toBeInstanceOf(Ship)
     })
+
     test("Ship sunk after three hits", () => {
         ship.hit()
         ship.hit()
         ship.hit()
         expect(ship.isSunk()).toBe(true);
     })
+
     test("Ship didn't sink after two hits, but then sinks", () => {
         ship.hit()
         ship.hit()
@@ -22,10 +26,63 @@ describe("Ships", () => {
         ship.hit()
         expect(ship.isSunk()).toBe(true);
     })
+
     test("Hitting after specified length", () => {
         ship.hit()
         ship.hit()
         ship.hit()
         expect(() => ship.hit()).toThrow("Ship already sank")
+    })
+})
+
+describe("Board", () => {
+    let board;
+    // [<y-cord>, <x-cord>, <ship-size>]
+    let ships = [[0, 0, 5], [2, 2, 4], [8, 6, 3], [9, 6, 3], [5, 8, 2]]
+    beforeEach(() => {
+        board = new GameBoard()
+        for (let s of ships) board.place(...s);
+    })
+
+    test("Positive ship validation", () => {
+        for (let n of ships) {
+            expect(board.isShip(n[0], n[1])).toBe(true)
+        }
+    })
+
+    test("Negative ship validation", () => {
+        expect(board.isShip(0, 6)).toBe(false)
+        expect(board.isShip(2, 6)).toBe(false)
+    })
+
+    test("Non direct ship validation", () => {
+        expect(board.isShip(0, 1)).toBe(true)
+        expect(board.isShip(0, 4)).toBe(true)
+        expect(board.isShip(2, 3)).toBe(true)
+    })
+
+    test("Invalid placement (wrong coordinates)", () => {
+        expect(() => board.place(0, 10, 2)).toThrow("Wrong ship coordinates")
+        expect(() => board.place(0, -10, 2)).toThrow("Wrong ship coordinates")
+    })
+
+    test("Invalid placement (invalid ship length)", () => {
+        expect(() => board.place(0, 8, 5)).toThrow("Wrong ship length")
+        expect(() => board.place(0, 9, -2)).toThrow("Wrong ship length")
+    })
+
+    test("Invalid placement (direct occupied cell)", () => {
+        expect(() => board.place(2, 5, 1)).toThrow("Cell already occupied")
+        expect(() => board.place(8, 6, 2)).toThrow("Cell already occupied")
+    })
+
+    test("Invalid placement (indirect occupied cell)", () => {
+        expect(() => board.place(5, 7, 2)).toThrow("Cell already occupied")
+        expect(() => board.place(2, 0, 3)).toThrow("Cell already occupied")
+    })
+
+    test("Ship validation after indirect invalid placement", () => {
+        expect(() => board.place(5, 7, 2)).toThrow("Cell already occupied")
+        expect(board.isShip(5, 7)).toBe(false)
     })
 })
