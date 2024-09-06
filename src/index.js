6d3c004a7d8403bc4188d7a3b89f31412399cf56
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-06 03:32:06 +0200

    Make guessing turns interchangeable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import "./load.js"
 import { leftGrid, rightGrid } from "./ui-controller.js"
 import { Player } from "./logic.js";
 
+export class Turn {
+    constructor (ply1, ply2) {
+        this.left = ply1;
+        this.right = ply2;
+        this.next = ply1;
+    }
+
+    isLeftTurn () {
+        return this.next === this.left
+    }
+
+    getNext () {
+        return this.next
+    }
+
+    getNextAttacked () {
+        return this.next == this.left ? this.right : this.left
+    }
+
+    changeTurn () {
+        this.next = this.next === this.left ? this.right : this.left
+    }
+}
+
 const ply1 = {
     logic: new Player(),
     grid: leftGrid
@@ -12,6 +36,8 @@ const ply2 = {
     logic: new Player(),
     grid: rightGrid
 }
+
+const turn = new Turn(ply1, ply2)
 
 // Predetermined ships values
 const leftShips = [[0, 0, 5], [2, 2, 4, true], [8, 6, 3], [9, 6, 3], [5, 8, 2, true]]
@@ -27,5 +53,15 @@ for (let j = 0; j < 5; j++) {
     ply2.grid.placeShip(...rightShips[j])
 }
 
-ply1.grid.placeEmpty(0, 9)
-ply2.grid.placeEmpty(9, 9)
+export function placeFromEvent (m, n, left) {
+    if (turn.isLeftTurn() !== left) {
+        try {
+            const ply = turn.getNextAttacked()
+            if (ply.logic.board.receiveAttack(m, n)) ply.grid.reviewShip(m, n)
+            else ply.grid.reviewEmpty(m, n)
+            turn.changeTurn()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
--- src/ui-controller.js
+++ src/ui-controller.js
@@ -1,4 +1,13 @@
+import { placeFromEvent } from "."
 import emptyUrl from "../media/cross.svg"
+
+document.querySelectorAll(".cell").forEach((cell) => {
+    cell.addEventListener("click", () => {
+        // Last arguments represents witch cell was clicked, left or right side one
+        // True for left
+        placeFromEvent(+cell.getAttribute("data-row"), +cell.getAttribute("data-col"), document.getElementById("left-playing-div").contains(cell))
+    })
+})
 
 class GridController {
     constructor (selectedDiv) {
@@ -12,7 +21,7 @@ class GridController {
             cell.classList.add("ship")
         }
     }
-    placeEmpty (m, n) {
+    reviewEmpty (m, n) {
         const cell = this.div.querySelector(`[data-row="${m}"][data-col="${n}"]`)
         const emptyImg = document.createElement("img");
 
@@ -20,6 +29,11 @@ class GridController {
 
         cell.appendChild(emptyImg)
     }
+    reviewShip (m, n) {
+        const cell = this.div.querySelector(`[data-row="${m}"][data-col="${n}"]`)
+        cell.classList.add("ship");
+    }
+
 }
 
 export const leftGrid = new GridController(document.querySelector("#left-playing-div .main-grid-div"))
